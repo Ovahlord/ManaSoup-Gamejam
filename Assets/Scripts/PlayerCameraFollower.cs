90c76d6f667adef29ad574bc6e0abbda64c258a0
Ovahlord <dreadkiller@gmx.de>
2024-03-22 21:21:50 +0100

    Scripts/Players: initial work on character splitting

--- Assets/Scripts/PlayerCameraFollower.cs
+++ Assets/Scripts/PlayerCameraFollower.cs
@@ -4,27 +4,62 @@ using UnityEngine;
 
 public class PlayerCameraFollower : MonoBehaviour
 {
-    [SerializeField] private float followLerpSpeed = 5.0f;
-    [SerializeField] private float cameraDistance = 10f;
-    [SerializeField] private float cameraHeightFacingOffset = 1f;
-    public Transform TargetTransform = null;
 
+    private float followLerpFactor = 5.0f;
+    private float cameraDistance = 10f;
+    private float cameraHeightFacingOffset = 1f;
     private Vector3 cameraOffset = Vector3.zero;
     private Vector3 heightFacingOffset = Vector3.zero;
+    private Transform targeTransform = null;
+
+    private static PlayerCameraFollower instance = null;
 
     private void Awake()
     {
         cameraOffset = Quaternion.Euler(45f, 0f, 0f) * (Vector3.back * cameraDistance);
         heightFacingOffset = new(0f, cameraHeightFacingOffset, 0f);
+
+        if (instance != null)
+            Destroy(instance);
+
+        instance = this;
     }
 
     void Update()
     {
-        if (TargetTransform == null)
+        if (Target == null)
             return;
 
-        transform.position = Vector3.Lerp(transform.position, TargetTransform.position, Time.deltaTime * followLerpSpeed);
+        transform.position = Vector3.Lerp(transform.position, Target.position, Time.deltaTime * FollowLerpFactor);
         Camera.main.transform.position = transform.position + cameraOffset;
         Camera.main.transform.LookAt(transform.position + heightFacingOffset);
+    }
+
+    public static Transform Target
+    {
+        get { return instance.targeTransform; }
+        set { instance.targeTransform = value; }
+    }
+
+    public static float FollowLerpFactor
+    {
+        get { return instance.followLerpFactor; }
+        set { instance.followLerpFactor = value; }
+    }
+
+    public static float CameraDistance
+    {
+        get { return instance.cameraDistance; }
+        set { instance.cameraDistance = value; }
+    }
+
+    public static float CameraFacingHeightOffset
+    {
+        get { return instance.cameraHeightFacingOffset; }
+        set
+        {
+            instance.cameraHeightFacingOffset = value;
+            instance.heightFacingOffset = new(0f, value, 0f);
+        }
     }
 }
--- Assets/Scripts/PlayerCharacterController.cs
+++ Assets/Scripts/PlayerCharacterController.cs
@@ -8,18 +8,26 @@ public class PlayerCharacterController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float bodyTurnSpeed = 720f;
+    [SerializeField] private float cameraFollowLerpFactor = 5.0f;
+    [SerializeField] private float cameraDistance = 10f;
+    [SerializeField] private float cameraFacingHeightOffset = 1f;
+    [SerializeField] private List<GameObject> splittingCharacters = new List<GameObject>();
 
     private CharacterController characterController = null;
     private Vector2 moveInputValue = Vector2.zero;
     private Vector3 motion = Vector3.zero;
     private float targetBodyRotation = 0f;
+    private List<GameObject> splittedCharacters = new List<GameObject>();
 
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
-        GameObject cameraFollower = new("CameraFollowe");
-        PlayerCameraFollower follower = cameraFollower.AddComponent<PlayerCameraFollower>();
-        follower.TargetTransform = transform;
+        new GameObject("CameraFollower").AddComponent<PlayerCameraFollower>();
+
+        PlayerCameraFollower.Target = transform;
+        PlayerCameraFollower.CameraDistance = cameraDistance;
+        PlayerCameraFollower.FollowLerpFactor = cameraFollowLerpFactor;
+        PlayerCameraFollower.CameraFacingHeightOffset = cameraFacingHeightOffset;
     }
 
     // Start is called before the first frame update
@@ -40,10 +48,34 @@ public class PlayerCharacterController : MonoBehaviour
         characterController.Move(motion);
     }
 
-    void OnMove(InputValue value)
+    public void OnMove(InputValue value)
     {
+        Debug.Log("OnMove has been invoked :)");
+
         moveInputValue = value.Get<Vector2>();
         if (moveInputValue.magnitude != 0f)
             targetBodyRotation = Mathf.Atan2(moveInputValue.x, moveInputValue.y) * Mathf.Rad2Deg;
+    }
+
+    public void OnSplit(InputValue value)
+    {
+        Debug.Log("splitting...");
+
+        // We already have active splitted characters. Destroy them...
+        if (splittedCharacters.Count > 0)
+        {
+            foreach (GameObject character in splittedCharacters)
+                Destroy(character);
+            return;
+        }
+
+        //... otherwise spawn new
+        foreach (GameObject splittingCharacter in splittingCharacters)
+            SpawnCharacter(splittingCharacter);
+    }
+
+    private void SpawnCharacter(GameObject character)
+    {
+
     }
 }
